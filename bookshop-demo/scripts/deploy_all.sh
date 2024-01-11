4b365c9861c62fcb29cd54a4e84d99778b1b1598
tukki <tukki0210@gmail.com>
2024-01-11 11:45:38 +0900

    ローカルのイメージを使うように変更

--- bookshop-demo/scripts/deploy_all.sh
+++ bookshop-demo/scripts/deploy_all.sh
@@ -10,11 +10,20 @@ kubectl apply -f bff/k8s/bff.yaml
 
 kind load docker-image gihyo-ms-dev-book/catalogue:0.1
 kubectl apply -f catalogue/k8s/catalogue.yaml
+
+kind load docker-image gihyo-ms-dev-book/catalogue-db:0.1
 kubectl apply -f catalogue/k8s/catalogue-db.yaml
 
+
+kind load docker-image gihyo-ms-dev-book/order:0.1
 kubectl apply -f order/k8s/order.yaml
+
+kind load docker-image gihyo-ms-dev-book/order-db:0.1
 kubectl apply -f order/k8s/order-db.yaml
+
+kind load docker-image gihyo-ms-dev-book/shipping:0.1
 kubectl apply -f shipping/k8s/shipping.yaml
+
 kubectl apply -f rabbitmq/k8s/rabbitmq.yaml
 
 echo "wait..."
